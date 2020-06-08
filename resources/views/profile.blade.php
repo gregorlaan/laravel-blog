5035388edddc9a13081e41927adcf43355c2d295
gregorlaan <laangregor@gmail.com>
2020-06-08 10:42:17 -0700

    Create related user profile with database migration

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,6 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1>{{ $user->username }}</h1>
+
+            <p class="pt-4 font-weight-bold">{{ $user->profile->title }}</p>
+
+            <p>{{ $user->profile->description }}</p>
         </div>
     </div>
 </div>
