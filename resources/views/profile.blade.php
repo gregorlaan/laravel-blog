d7aea63bd53f0f562e21341853170d555f717ece
Gregor Laan <laangregor@gmail.com>
2020-06-10 11:44:25 +0300

    Get profile title and description

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,9 +6,9 @@
         <div class="col-md-8">
             <h1>{{ $user->username }}</h1>
 
-            <p class="pt-4 font-weight-bold">{{ $user->profile->title }}</p>
+            <p class="pt-4 font-weight-bold">{{ $user->profile->title}}</p>
 
-            <p>{{ $user->profile->description }}</p>
+            <p>{{ $user->profile->description}}</p>
         </div>
     </div>
 </div>
