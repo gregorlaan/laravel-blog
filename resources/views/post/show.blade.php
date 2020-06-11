12359d46a014193501398eb05edea31a076c6a30
Gregor Laan <laangregor@gmail.com>
2020-06-11 17:41:59 +0300

    Improve post single view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,13 +3,34 @@
 @section('content')
 <div class="container">
 
-    <div class="jumbotron">
-        <h1 class="display-4">{{ $post->title }}</h1>
-        <hr class="my-4">
-        <p class="lead">{{ $post->description }}</p>
+    <div class="shadow-sm p-3 mb-4 bg-white rounded-lg">
+        <div class="media">
+            @if ($post->thumbnail)
+                <img width="200" height="200" class="img-thumbnail align-self-start mr-3" src="/storage/{{ $post->thumbnail }}" alt="{{ $post->title }}">
+            @endif
+
+            <div class="media-body">
+                <h1>{{ $post->title }}</h1>
+                <hr class="my-3">
+                <p class="lead text-secondary">{{ $post->description }}</p>
+
+                <div class="media">
+                    <div style="width: 50px; height: 50px; background: white;" class="border rounded-circle figure-img d-inline-block" alt="placeholder"></div>
+
+                    <div class="media-body ml-2">
+                        <a href="/profile/{{$post->user->id}}" class="text-decoration-none">
+                            <h6 class="mt-0 font-weight-bold">{{ $post->user->name }}</h6>
+                        </a>
+
+                        <p>{{ $post->created_at }}</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 
-    <p>{{ $post->content }}</p>
+    <p class="lead">{{ $post->content }}</p>
 
 </div>
 @endsection
