21710ff2da449e0eabba7dec2998016f39c27262
Gregor Laan <laangregor@gmail.com>
2020-06-12 14:27:00 +0300

    Create ProfilePolicy to toggle user profile buttons visibility

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -10,30 +10,33 @@
         <hr class="my-2">
         <p>{{ $user->profile->description }}</p>
 
-        <p class="lead">
-            <a href="/post/create" id="add-new-post" class="btn btn-primary font-weight-bold">Add New Post</a>
-            <a href="/profile/edit" id="add-new-post" class="btn btn-outline-primary font-weight-bold px-4">Edit Profile</a>
-        </p>
+        @can('update', $user->profile)
+            <p class="lead">
+                <a href="/post/create" id="add-new-post" class="btn btn-primary font-weight-bold">Add New Post</a>
+                <a href="/profile/edit" id="add-new-post" class="btn btn-outline-primary font-weight-bold px-4">Edit Profile</a>
+            </p>
+        @endcan
+
     </div>
 
     <div class="row">
-            @foreach ($user->posts as $post)
-                <div class="col-md-4 mb-5">
-                    <div class="card">
-                        @if ($post->thumbnail)
-                            <img class="card-img-top" src="/storage/{{ $post->thumbnail }}" alt="{{ $post->title }}">
-                        @endif
+        @foreach ($user->posts as $post)
+            <div class="col-md-4 mb-5">
+                <div class="card">
+                    @if ($post->thumbnail)
+                        <img class="card-img-top" src="/storage/{{ $post->thumbnail }}" alt="{{ $post->title }}">
+                    @endif
 
-                        <div class="card-body">
-                            <a href="/post/{{$post->id}}" id="post-{{$post->id}}" class="text-decoration-none">
-                                <h4 class="card-title">{{ $post->title }}</h4>
-                            </a>
+                    <div class="card-body">
+                        <a href="/post/{{$post->id}}" id="post-{{$post->id}}" class="text-decoration-none">
+                            <h4 class="card-title">{{ $post->title }}</h4>
+                        </a>
 
-                            <p class="card-text">{{ $post->description }}</p>
-                        </div>
+                        <p class="card-text">{{ $post->description }}</p>
                     </div>
                 </div>
-            @endforeach
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
