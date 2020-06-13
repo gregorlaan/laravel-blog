846fb0b239e074f00b11f8172d635eeb48e98355
Gregor Laan <laangregor@gmail.com>
2020-06-13 23:36:43 +0300

    Optimize query - Get following users with one query

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
     {
         $users = auth()->user()->following()->pluck('profiles.user_id');
 
-        $posts = Post::whereIn('user_id', $users)->latest()->paginate(5);
+        $posts = Post::whereIn('user_id', $users)->with('user')->latest()->paginate(5);
 
         return view('post.index', compact('posts'));
     }
